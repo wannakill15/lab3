8972affaf64b5783c01f9c59d2912040223705b7
wannakill15 <ken.ae26@gmail.com>
2024-03-04 11:20:20 +0800

    Update index.php

--- lab 3/index.php
+++ lab 3/index.php
@@ -23,7 +23,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         exit(); 
         //checking the user input on the database if exist
     } else {
-        $sql = "SELECT * FROM user WHERE username='$uname' AND password='$pass'";
+        $sql = "SELECT * FROM user_profile WHERE username='$uname' AND password='$pass'";
         $result = mysqli_query($conn, $sql);
         if (mysqli_num_rows($result) === 1) {
             $row = mysqli_fetch_assoc($result);
